a80d238b085c843342734a4be3f22d87d670ce30
shogoisaji <simacsimac9@gmail.com>
2024-12-25 14:06:36 +0900

    fi

--- memo.tsx
+++ memo.tsx
@@ -1,15 +1,16 @@
-Sub RemoveDuplicateRows()
+Sub FilterByAColumn()
     Dim ws As Worksheet
     Dim lastRow As Long
     
     ' 対象シートを設定
     Set ws = Worksheets("指定したシート名") ' シート名を指定してください
     
-    ' A列からC列のデータが入力されている最終行を取得
+    ' A列の最終行を取得
     lastRow = ws.Cells(ws.Rows.Count, "A").End(xlUp).Row
     
-    ' 重複を削除
+    ' フィルターを設定
     With ws
-        .Range("A1:C" & lastRow).RemoveDuplicates Columns:=Array(1, 2, 3), Header:=xlYes
+        .AutoFilterMode = False ' 既存のフィルターを解除
+        .Range("A1:A" & lastRow).AutoFilter Field:=1, Criteria1:="A-3"
     End With
 End Sub
