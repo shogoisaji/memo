0d16ae70727741f0b0473e40481cf3d16fa1870b
shogoisaji <simacsimac9@gmail.com>
2025-01-21 14:40:40 +0900

    res

--- memo copy.tsx
+++ memo copy.tsx
@@ -1,13 +1,24 @@
-const applicableBaseDateString = applicableBaseDate.toISOString();
+import { Box } from "@mui/material";
 
-newFieldValues[i] = {
-  ...visitFields[i],
-  visitId: values.visitId,
-  applyDate: applicableBaseDateString,
-};
-console.log(applicableBaseDateString);
-
-return {
-  ...values,
-  applicableBaseDateString,
-};
+function ResponsiveImage({ src, alt }) {
+  return (
+    <Box
+      component="img"
+      src={src}
+      alt={alt}
+      sx={{
+        width: {
+          xs: "0", // 非表示
+          sm: "200px",
+          md: "300px",
+          lg: "400px",
+        },
+        height: "auto",
+        display: {
+          xs: "none",
+          sm: "block",
+        },
+      }}
+    />
+  );
+}
